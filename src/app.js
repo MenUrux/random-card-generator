35835e091d4035830fef968b0ab84d1cc5293ee2
menurux <jorgepino.utu@gmail.com>
2024-07-04 10:27:00 -0300

    refactorized js code

--- src/app.js
+++ src/app.js
@@ -1,86 +1,77 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const cardDom = document.getElementById("card");
-const generateCardButton = document.getElementById("generate-card");
-const typeDom = document.querySelectorAll(".type");
-const widthInput = document.getElementById("width");
-const heightInput = document.getElementById("height");
+// this waits to get the DOM loaded before doing something.
+document.addEventListener("DOMContentLoaded", () => {
+  const cardDom = document.getElementById("card");
+  const generateCardButton = document.getElementById("generate-card");
+  const typeDom = document.querySelectorAll(".type");
+  const widthInput = document.getElementById("width");
+  const heightInput = document.getElementById("height");
 
-/* console.log(typeDom); */
+  const cardArray = [
+    "A",
+    "K",
+    "J",
+    "Q",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
+  const typeArray = ["♠️", "♣️", "♥️", "♦️"];
 
-let cardArray = [
-  "A",
-  "K",
-  "J",
-  "Q",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10"
-];
+  //refactorized arrowfunctions. 
+  // changed name for better legibility 
+  const getRandomElement = array =>
+    array[Math.floor(Math.random() * array.length)];
 
-/* let typeArray = ["spade", "club", "heart", "diamond"];
- */
-let typeArray = ["♠️", "♣️", "♥️", "♦️"];
+  const generateCard = () => {
+    // better way to erase all the classes
+    cardDom.className = "";
 
-function randomNumber(array) {
-  let random = Math.floor(Math.random() * array.length);
-  return random;
-}
+    const cardValue = getRandomElement(cardArray);
+    const typeValue = getRandomElement(typeArray);
 
-function generateCard() {
-  cardDom.classList.value = "";
-
-  let cardValue = cardArray[randomNumber(cardArray)];
-  let typeValue = typeArray[randomNumber(typeArray)];
-
-  cardDom.innerHTML = `
-    <span class="type">${typeValue}</span>
-    <div id="card-number">${cardValue} </div>
-    <span class="type">${typeValue}</span>
+    cardDom.innerHTML = `
+      <span class="type">${typeValue}</span>
+      <div id="card-number">${cardValue}</div>
+      <span class="type">${typeValue}</span>
     `;
 
-  cardDom.classList.add(typeValue);
+    cardDom.classList.add(typeValue);
 
-  typeDom.forEach(type => {
-    type.innerText = typeValue;
-  });
-}
+    typeDom.forEach(type => {
+      type.innerText = typeValue;
+    });
+  };
 
-window.onload = function() {
+  const changeSize = () => {
+    const width = parseInt(widthInput.value, 10);
+    const height = parseInt(heightInput.value, 10);
+
+    // modified the width <= to be as the description, (before was <100).
+    if (!isNaN(width) && width <= 100 && width >= 15) {
+      cardDom.style.width = `${width}rem`;
+    }
+
+    if (!isNaN(height) && height <= 100 && height >= 10) {
+      cardDom.style.height = `${height}rem`;
+    }
+  };
+
+  generateCardButton.addEventListener("click", generateCard);
+  widthInput.addEventListener("change", changeSize);
+  heightInput.addEventListener("change", changeSize);
+
   generateCard();
-};
-
-function changeSize(newValueSize) {
-  console.log(newValueSize);
-
-  const width = parseInt(widthInput.value, 10);
-  const height = parseInt(heightInput.value, 10);
-
-  if (!isNaN(width) && width < 100 && width >= 15) {
-    card.style.width = width + "rem";
-  }
-
-  if (!isNaN(height) && height < 100 && height >= 10) {
-    card.style.height = height + "rem";
-  }
-}
-
-generateCardButton.onclick = generateCard;
-
-widthInput.addEventListener("change", changeSize);
-heightInput.addEventListener("change", changeSize);
-
-setInterval(() => {
-  generateCard();
-}, 10000);
+  setInterval(generateCard, 10000);
+});
